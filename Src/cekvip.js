32c04a63d24d68fe764fff74abd5f0a93d8eacd6
ysaias096 <76224312+ysaias096@users.noreply.github.com>
2021-02-24 21:21:24 -0300

    Add files via upload

---
+++ Src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nome do bot* :  🐊⃟𝜝𝚯𝑇  𝒀𝑺𝛥𝑰𝛥𝑺🔥
+──────────────────
+        『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*AÍ SIM PAE, VOCÊ É UM MEMBRO VIP😎✋`
+}
+exports.cekvip = cekvip
---
+++ Src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *PIC PAY E PIX *
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/5575981161565*
+║
+╚═〘 YSAIAS BOT 〙
+`
+}
+exports.iklan = iklan
